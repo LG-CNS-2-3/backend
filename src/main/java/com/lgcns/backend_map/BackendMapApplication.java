4d203054c2d3c6d0fce5c49ce800b37ac81439a1
khw7385 <khw7385@naver.com>
2025-10-23 14:35:10 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/lgcns/backend_map/BackendMapApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.backend_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMapApplication.class, args);
+	}
+
+}
