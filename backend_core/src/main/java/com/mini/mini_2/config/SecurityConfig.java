94ae0dacabd840fd3544e9f4a7490b72f17ed662
khw7385 <khw7385@naver.com>
2025-10-28 01:44:37 +0900

    feat: security 설정

---
+++ backend_core/src/main/java/com/mini/mini_2/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.mini.mini_2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers(
+                                "/api/v1/members/login",
+                                "/api/v1/members/join",
+                                "/api/v1/mini/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                ).build();
+        return http.build();
+    }
+}
