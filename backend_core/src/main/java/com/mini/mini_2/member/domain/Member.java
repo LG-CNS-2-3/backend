49e6796352c5e596afa09c36471b38707ab418d1
MinJaeSon <mjsonsj@gmail.com>
2025-10-27 15:15:10 +0900

    fix: 회원 테이블명 수정

--- backend_core/src/main/java/com/mini/mini_2/member/domain/Member.java
+++ backend_core/src/main/java/com/mini/mini_2/member/domain/Member.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "member")
+@Table(name = "user")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
