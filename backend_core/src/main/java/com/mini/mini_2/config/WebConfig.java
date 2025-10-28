bd15ca53e4125cdde54e8e3e889d403c165a2096
MinJaeSon <mjsonsj@gmail.com>
2025-10-28 16:29:01 +0900

    fix: web 설정 파일 수정

--- backend_core/src/main/java/com/mini/mini_2/config/WebConfig.java
+++ backend_core/src/main/java/com/mini/mini_2/config/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor)
+        registry.addInterceptor(userIdInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(
                         // "/api/v1/mini/openapi/**",
