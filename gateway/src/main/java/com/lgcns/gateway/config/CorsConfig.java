7592805f5e9b5725304853115f785c540cebb80d
khw7385 <khw7385@naver.com>
2025-10-28 23:55:40 +0900

    fix: cors 설정

--- gateway/src/main/java/com/lgcns/gateway/config/CorsConfig.java
+++ gateway/src/main/java/com/lgcns/gateway/config/CorsConfig.java
@@ -2,9 +2,19 @@ package com.lgcns.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
